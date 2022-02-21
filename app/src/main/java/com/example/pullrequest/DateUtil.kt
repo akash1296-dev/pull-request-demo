96d94daa3b492fdea1304f43546320d49d9fbad5
Aakash <ambi321ti0ns>
2022-02-22 01:01:18 +0530

    Code Optimization

--- app/src/main/java/com/example/pullrequest/DateUtil.kt
+++ app/src/main/java/com/example/pullrequest/DateUtil.kt
@@ -4,6 +4,9 @@ import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.util.*
 
+/**
+ * Created in order to format date to human readable format
+ */
 object DateUtil {
     private val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
     private val outputFormat = SimpleDateFormat("dd MMM yyyy");
